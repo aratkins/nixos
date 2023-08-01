918e4b77dd7cbead422619c5724bd4cc7ba19abb
Aaron Atkins <aaron.r.atkins@gmail.com>
2023-08-01 18:26:45 -0400

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,20 @@
+services = {
+xserver = {
+        enable = true;
+        displayManager = {
+          defaultSession = "none+i3";
+        };
+        excludePackages = [
+          pkgs.xterm
+        ];
+        windowManager.i3 = {
+          enable = true;
+          extraPackages = with pkgs; [
+            dmenu
+            i3status
+            i3lock
+            i3blocks
+          ];
+        };
+      };
+};
